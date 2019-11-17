ecef15c289b74a3ea19d8a45e24ef5fc82aa2494
Pumin Swangjang <fvck3oy@hotmail.com>
2019-11-17 11:07:48 +0700

    Updates

---
+++ precache-manifest.70e735f1b32b9fda60d9d131652d0b63.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe3a3f40494c92d0ce7499b6ec4c2ff",
+    "url": "/lawyer-app/index.html"
+  },
+  {
+    "revision": "2cbfec677ba0079e091d",
+    "url": "/lawyer-app/static/css/2.e76d2b5a.chunk.css"
+  },
+  {
+    "revision": "4c94f9241a576a582042",
+    "url": "/lawyer-app/static/css/main.0df3d602.chunk.css"
+  },
+  {
+    "revision": "2cbfec677ba0079e091d",
+    "url": "/lawyer-app/static/js/2.b8bc49fc.chunk.js"
+  },
+  {
+    "revision": "4c94f9241a576a582042",
+    "url": "/lawyer-app/static/js/main.79ae999b.chunk.js"
+  },
+  {
+    "revision": "7fa47103fe7d366478c4",
+    "url": "/lawyer-app/static/js/runtime-main.26ee4dd3.js"
+  },
+  {
+    "revision": "5221a1f1ed53a149256f3890e2fe5409",
+    "url": "/lawyer-app/static/media/image1.5221a1f1.png"
+  },
+  {
+    "revision": "f7294f169c55469516c3f49a296168f5",
+    "url": "/lawyer-app/static/media/image2.f7294f16.png"
+  },
+  {
+    "revision": "8785ecfb39288347fd23cbf4521dca27",
+    "url": "/lawyer-app/static/media/image3.8785ecfb.png"
+  },
+  {
+    "revision": "2b085c50b614d55d648ffb9a8c3c0c46",
+    "url": "/lawyer-app/static/media/image4.2b085c50.png"
+  },
+  {
+    "revision": "ae1760da0b40f3a6d23b0ce788f91de0",
+    "url": "/lawyer-app/static/media/logo.ae1760da.jpg"
+  },
+  {
+    "revision": "ee10e5bd83c2161a1ac3205a0da57a70",
+    "url": "/lawyer-app/static/media/qr_line.ee10e5bd.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/lawyer-app/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/lawyer-app/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/lawyer-app/static/media/revicons.2feb69cc.eot"
+  }
+]);
