8e633ece147eef9da6e16f22fe0adbe2871b3d00
Pumin Swangjang <fvck3oy@hotmail.com>
2020-01-13 23:44:25 +0700

    update pagination

--- src/pages/AllArticle.js
+++ src/pages/AllArticle.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import { Button, Card, Tag } from 'antd'
+import { Button, Card, Tag, Select, Form, Pagination } from 'antd'
 import axios from 'axios'
 import url from '../url_config'
 import { Link } from 'react-router-dom'
 import './AllArticle.css'
 import Moment from 'react-moment'
 const { Meta } = Card;
-
+const { Option } = Select;
 export default class AllArticle extends Component {
   state = {
-    data: []
+    data: [],
+    text:'ทั้งหมด',
+    value:0,
+    current:1,
+    total:0
   }
   componentDidMount = () => {
     this.getData()
   }
 
   getData = async () => {
-
-    await axios.get(`${url}/blogs`).then(res => {
+    await axios.get(`${url}/blogs/filter/0/0`).then(res => {
       const { data } = res
       this.setState({ data });
       console.log("DataImage : ", data);
+      this.setState({ total:data.length})
 
     })
   }
+  getFilter = async (value,page_number) => {
+    await axios.get(`${url}/blogs/filter/${value}/${page_number}`).then(res => {
+      const { data } = res
+      this.setState({ data });
+      console.log("DataFilter : ", data);
+
+    })
+  }
+
+  filter = (value, event) => {
+    this.getFilter(value)
+    if(value == 0){
+      this.setState({ value:0 })
+      this.setState({ text:'ทั้งหมด'})
+    }else if(value == 1){
+      this.setState({ value:1})
+      this.setState({ text:'ข่าว'})
+    }
+    else if(value == 2){
+      this.setState({ value:2 })
+      this.setState({ text:'บทความ'})
+    }
+    else{
+      this.setState({ value:3 })
+      this.setState({ text:'กิจกรรม'})
+    }
+    console.log("filter : ", value);
+  }
+
+  onPageChange=(value)=>{
+    console.log('PageChange : ', value);
+    this.getFilter(this.state.value,value-1)
+  }
+
   render() {
     const urlImage = "http://167.71.193.2:3001/"
     // const urlImage = "http://127.0.0.1:3001/"
@@ -35,7 +73,24 @@ export default class AllArticle extends Component {
           {/* <Col lg={{ size: 8, offset: 2 }} md={{ size: 10, offset: 1 }} sm={{ size:12}}> */}
           <Col md={12}>
             <div className="p-5">
-              <h3>ข่าว/บทความ/กิจกรรม (ทั้งหมด)</h3>
+              <h3>ข่าว / บทความ / กิจกรรม ({this.state.text})</h3>
+              <Row>
+                <Col md={12}>
+                  <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+                    <div>
+                      <Form >
+                        <Select onSelect={(value, event) => this.filter(value, event)} placeholder="กรุณาเลือกประเภทของบทความ !" defaultValue="0">
+                          <Option value="0">ทั้งหมด</Option>
+                          <Option value="1">ข่าว</Option>
+                          <Option value="2">บทความ</Option>
+                          <Option value="3">กิจกรรม</Option>
+                        </Select>
+                      </Form>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+
               <Row>
 
                 {
@@ -50,28 +105,28 @@ export default class AllArticle extends Component {
                     return (
                       <Col md={4} key={e.id}>
                         <div className="mt-2 mb-2">
-                        <Link to={`${urlArticle}${e.id}`}>
-                          <Card
-                            hoverable
-                            style={{ width: '100%', height: '100%' }}
-                            cover={<img alt="example" src={`${urlImage}${e.image}`} width="100%" height="200px" />}
-                          >
-                            <Meta
-                              title={e.title}
-                              description={e.detail}
-                            />
-                            <div className="read-more">
-                              <div className="p-2">
-                                <Tag color="#f90">
-                                  {e.type}
-                                </Tag>
-                                <Tag color="#f90"><Moment format="DD/MM/YYYY">{e.created}</Moment></Tag>
-                              </div>
-                              {/* <div className="p-2">
+                          <Link to={`${urlArticle}${e.id}`}>
+                            <Card
+                              hoverable
+                              style={{ width: '100%', height: '100%' }}
+                              cover={<img alt="example" src={`${urlImage}${e.image}`} width="100%" height="200px" />}
+                            >
+                              <Meta
+                                title={e.title}
+                                description={e.detail}
+                              />
+                              <div className="read-more">
+                                <div className="p-2">
+                                  <Tag color="#f90">
+                                    {e.type}
+                                  </Tag>
+                                  <Tag color="#f90"><Moment format="DD/MM/YYYY">{e.created}</Moment></Tag>
+                                </div>
+                                {/* <div className="p-2">
                                 <Link to={`${urlSaleLand}${e.id}`}> อ่านต่อ. . . </Link>
                               </div> */}
-                            </div>
-                          </Card>
+                              </div>
+                            </Card>
                           </Link>
                         </div>
                       </Col>
@@ -80,6 +135,7 @@ export default class AllArticle extends Component {
                 }
 
               </Row>
+              <Pagination defaultCurrent={1} defaultPageSize={10} total={this.state.total} onChange={this.onPageChange}/>
             </div>
 
           </Col>
